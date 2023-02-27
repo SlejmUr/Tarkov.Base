d55b8b2fb2c95309c0e2e4df6168a89faae7df17
SlejmUr <slejmur@protonmail.com>
2023-02-27 20:28:48 +0100

    TestMod

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Tarkov.Base
+
+Tarkov base Mod where most of the basic stuff could work
+
+
+# Thanks list
+- Everyone
---
+++ TestMod/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+
+namespace TestMod
+{
+    [BepInPlugin("TestMod", "TestMod", "0.1")]
+    [BepInDependency("TarkovBase","0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            Logger.LogMessage("say hi to the camera!");
+        }
+        private void Awake()
+        {
+            Tarkov.Base.Plugin.SceneLoadActions.Add(Test);
+        }
+
+        private void Test()
+        {
+            Logger.LogMessage("from the TestMod!");
+        }
+    }
+}
